d42b4764f7639524c7925abdba81ac9b3aa783f2
nathanmintegui <nathanberger.rs@hotmail.com>
2025-03-25 19:57:51 -0300

    styles: beautify submit buttons

--- client/enterprise/candidadate/src/app/modules/code-submit/code-submit.component.ts
+++ client/enterprise/candidadate/src/app/modules/code-submit/code-submit.component.ts
@@ -1,65 +1,78 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {CodeEditor} from "@acrodata/code-editor";
 import {FormsModule} from "@angular/forms";
 import {languages} from '@codemirror/language-data';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
-    selector: 'app-code-submit',
-    imports: [
-        CodeEditor,
-        FormsModule
-    ],
-    templateUrl: './code-submit.component.html',
-    styleUrl: './code-submit.component.css'
+  selector: 'app-code-submit',
+  imports: [
+    CodeEditor,
+    FormsModule
+  ],
+  templateUrl: './code-submit.component.html',
+  styleUrl: './code-submit.component.css'
 })
 export class CodeSubmitComponent {
-    value = `/**
- * Sorts an array using the QuickSort algorithm.
- *
- * QuickSort is a divide-and-conquer algorithm that selects a pivot element,
- * partitions the array into two subarrays (elements less than the pivot and elements greater),
- * and recursively sorts each subarray.
- *
- * @param {number[]} arr - The array of numbers to be sorted.
- * @returns {number[]} A new sorted array.
- *
- * @example
- * const sorted = quickSort([3, 6, 8, 10, 1, 2, 1]);
- * console.log(sorted); // Output: [1, 1, 2, 3, 6, 8, 10]
- */
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr; // Base case: arrays with 0 or 1 element are already sorted
+  value =
+    `function twoSum(nums, target) {
+      const map = new Map();
+      for (let i = 0; i < nums.length; i++) {
+          const complement = target - nums[i];
+          if (map.has(complement)) {
+              return [map.get(complement), i];
+          }
+          map.set(nums[i], i);
+      }
+      return [];
     }
 
-    const pivot = arr[arr.length - 1]; // Choose the last element as the pivot
-    const left = [];  // Elements less than the pivot
-    const right = []; // Elements greater than or equal to the pivot
+module.exports = twoSum;
+`;
+  languages = languages
+  options: any = {
+    language: 'javascript',
+    theme: 'light',
+    setup: 'basic',
+    disabled: false,
+    readonly: false,
+    placeholder: 'Type your code here...',
+    indentWithTab: false,
+    indentUnit: '',
+    lineWrapping: false,
+    highlightWhitespace: false,
+  };
+  userCode = signal('');
 
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    // Recursively sort the left and right subarrays and concatenate them with the pivot
-    return [...quickSort(left), pivot, ...quickSort(right)];
-}`;
+  log(e: any) {
+    this.userCode.set(e)
+  }
 
-    languages = languages
-
-    options: any = {
-        language: 'javascript',
-        theme: 'light',
-        setup: 'basic',
-        disabled: false,
-        readonly: false,
-        placeholder: 'Type your code here...',
-        indentWithTab: false,
-        indentUnit: '',
-        lineWrapping: false,
-        highlightWhitespace: false,
+  handleClick = () => {
+    const body = {
+      lang: 1,
+      code: this.userCode()
     };
+
+    this.http.post('http://localhost:5002/questions/5353aedc-c178-4677-a9dd-53cb2644a078/submit', body, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).subscribe((res) => {
+      if (res === null) {
+        const eventSource = new EventSource('http://localhost:3001/events');
+        eventSource.onmessage = (event) => {
+          const {type, data} = JSON.parse(event.data);
+          if (type === 'close') {
+            eventSource.close();
+          } else {
+            console.log(data);
+          }
+        };
+      }
+    });
+  }
 }
